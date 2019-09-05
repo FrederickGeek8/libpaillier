2f50b9a5208b31a090fa71c07879de9eace0afc2
Frederick Morlock <FrederickGeek8@gmail.com>
2019-09-06 01:26:57 +0800

    Whoops forgot this

---
+++ declaration.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <float.h>
+#include <math.h>
+
+class PaillierPublicKey;
+class EncryptedNumber;
+class EncodedNumber;
+
+#ifndef include
+#define include
+#include "public_key.h"
+#include "encoded_number.h"
+#include "encrypted_number.h"
+#include "utils.h"
+#endif
