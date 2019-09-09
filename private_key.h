9762d31fcc7c644f951328b467bb2e09fec77129
Frederick Morlock <FrederickGeek8@gmail.com>
2019-09-09 19:19:16 +0800

    More private key stuff

--- private_key.h
+++ private_key.h
@@ -16,6 +16,13 @@ class PaillierPrivateKey {
         long hq;
 
         static PaillierPrivateKey from_totient(PaillierPublicKey* public_key, long totient);
+        float decrypt(EncryptedNumber& encrypted_number);
+        float decrypt_encoded(EncryptedNumber& encrypted_number);
+        long raw_decrypt(long ciphertext);
+        long h_function(long x, long x_square);
+        long l_function(long x, long p);
+        long crt(long mp, long mq);
+        
 }
 
 #endif
